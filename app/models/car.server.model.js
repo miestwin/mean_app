b29b90931e521d75ddc1393669ed2888e3b9d6bc
Radosław Chełstowski <chelstowski.radoslaw@gmail.com>
2016-12-28 14:54:36 +0100

    car model

---
+++ app/models/car.server.model.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var CarSchema = new Schema({
+    car_body: {
+        type: String,
+        default: 'No data',
+        trim: true
+    },
+    engine: {
+        type: Number,
+        default: 0,
+        min: 0  
+    },
+    color: {
+        type: String,
+        default: 'No data',
+        trim: true
+    },
+    course: {
+        type: Number,
+        default: 0,
+        min: 0
+    },
+    car_model: {
+        type: String,
+        default: 'No data',
+        trim: true
+    },
+    car_brand: {
+        type: String,
+        default: 'No data',
+        trim: true
+    },
+    data_of_production: {
+        type: Date,
+        default: Date.now
+    },
+    equipment: {
+        type: Array
+    }
+});
+
+mongoose.model('Car', CarSchema);
