e7035d7f299209ae14d4ccaf7efeeecb064a7bb2
Radosław Chełstowski <chelstowski.radoslaw@gmail.com>
2016-12-29 15:00:03 +0100

    create empty routes

--- app/routes.js
+++ app/routes.js
@@ -3,18 +3,59 @@ var Car = require('./models/car.server.model'),
 
 module.exports = function(app, dirname) {
 
-    app.get('/api/cars', function(req, res) {
+    //list cars
+    app.get('/', function(req, res) {
         Car.find(function(err, cars) {
             if(err) res.send(err);
             res.json(cars);
         });
     });
 
-    app.get('/api/equipments', function(req, res) {
+    //show car :id
+    app.get('/cars/:id', function(req, res) {
+
+    });
+
+    //create new car
+    app.post('/cars', function(req, res) {
+
+    });
+
+    //update car:id
+    app.put('/cars/:id', function(req, res) {
+
+    });
+
+    //delete car:id
+    app.delete('/cars/"id', function(req, res) {
+
+    });
+
+    //list equipments
+    app.get('/equipments', function(req, res) {
         Equipment.find(function(err, cars) {
             if(err) res.send(err);
             res.json(cars);
         });
+    });
+
+    //show equipment:id
+    app.get('/equipments/:id', function(req, res) {
+
+    });
+
+    app.post('/equipments', function(req, res) {
+
+    });
+
+    //update car:id
+    app.put('/equipments/:id', function(req, res) {
+
+    });
+
+    //delete car:id
+    app.delete('/equipments/"id', function(req, res) {
+
     });
 
     app.get('*', function(req, res) {
