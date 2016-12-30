eb8bb8487d1ff7d4b3721cd0678e04020f00281e
Radosław Chełstowski <chelstowski.radoslaw@gmail.com>
2016-12-30 19:25:43 +0100

    better routes

--- app/routes.js
+++ app/routes.js
@@ -5,9 +5,11 @@ module.exports = function(app, dirname) {
 
     //list cars
     app.get('/', function(req, res) {
-        Car.find(function(err, cars) {
-            if(err) res.send(err);
-            res.json(cars);
+        Car.find({})
+            .populate("equipment")
+            .exec(function(err, cars) {
+                if(err) res.send({ response: err, status: 'ERROR'});
+                res.json({ response: cars, status: 'OK' });
         });
     });
 
@@ -15,80 +17,82 @@ module.exports = function(app, dirname) {
         Car.find({})
             .populate("equipment")
             .exec(function(err, cars) {
-            if(err) res.send(err);
-            res.json(cars);
+                if(err) res.send({ response: err, status: 'ERROR'});
+                res.json({ response: cars, status: 'OK' });
         });
     });
 
     //show car :id
     app.get('/cars/:id', function(req, res) {
-        Car.findById(req.params.id, function(err, car) {
-            if(err) res.json(err);
-            res.json(car);
+        Car.findById(req.params.id)
+            .populate("equipment")
+            .exec(function(err, car) {
+                if(err) res.json({ response: [], status: 'ERROR', error: err});
+                res.json({ response: car, status: 'OK' });
         });
     });
 
     //create new car
     app.post('/cars', function(req, res) {
         Car.create(req.body, function(err, car) {
-            if(err) res.json(err);
-            res.json(car);
+            if(err) res.json({ message: 'ERROR', error: err});
+            res.json({ message: 'OK' });
         });
     });
 
     //update car:id
     app.put('/cars/:id', function(req, res) {
         Car.findByIdAndUpdate(req.params.id, req.body, function(err, car) {
-            if(err) res.json(err);
-            res.json(car);
+            if(err) res.json({ message: 'ERROR', error: err});
+            res.json({ message: 'OK' });
         });
     });
 
     //delete car:id
     app.delete('/cars/"id', function(req, res) {
         Car.findByIdAndRemove(req.params.id, req.body, function(err, car) {
-            if(err) res.json(err);
-            res.json(car);
+            if(err) res.json({ message: 'ERROR', error: err});
+            res.json({ message: 'OK'});
         });
     });
 
     //list equipments
     app.get('/equipments', function(req, res) {
         Equipment.find(function(err, eq) {
-            if(err) res.send(err);
-            res.json(eq);
+            if(err) res.send({ response: err, status: 'ERROR'});
+            res.json({ response: eq, status: 'OK'});
         });
     });
 
     //show equipment:id
     app.get('/equipments/:id', function(req, res) {
         Equipment.findById(req.params.id, function(err, eq) {
-            if(err) return res.json(err);
-            res.json(eq);
+            if(err) return res.json({ response: err, status: 'ERROR'});
+            res.json({ response: eq, status: 'OK'});
         });
     });
 
     //create new equipment
     app.post('/equipments', function(req, res) {
         Equipment.create(req.body, function(err, eq) {
-            if(err) return res.json(err);
-            res.json(eq);
+            if(err) return res.json({ message: 'ERROR', error: err});
+            res.json({ message: 'OK'});
         });
     });
 
     //update equipment:id
     app.put('/equipments/:id', function(req, res) {
         Equipment.findByIdAndUpdate(req.params.id, req.body, function(err, eq) {
-            if(err) res.json(err);
-            res.json(eq);
+            if(err) res.json({ message: 'ERROR', error: err});
+            res.json({ message: 'OK'});
         });
     });
 
     //delete equipment:id
     app.delete('/equipments/"id', function(req, res) {
         Equipment.findByIdAndRemove(req.params.id, req.body, function(err, eq) {
-            if(err) res.json(err);
-            res.json({ response: [], status: "OK" });
+            if(err) res.json({ message: 'ERROR', error: err});
+            res.json({ message: 'OK' });
         });
     });
 
