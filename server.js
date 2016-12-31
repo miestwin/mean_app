997a0dd0c2c00bfdafd8bc1c344420d6e7b7fc01
Radosław Chełstowski <chelstowski.radoslaw@gmail.com>
2016-12-31 17:23:32 +0100

    page 404 added

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ var mongoose = require('mongoose'),
     database = require('./config/database');
 mongoose.Promise = global.Promise;
 
-// var Car = require('./app/models/car.server.model'),
-//     Equipment = require('./app/models/equipment.server.model');
-
 var port = process.env.PORT || 8080;
 
 mongoose.connect(database.url);
@@ -23,31 +20,6 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
 require('./app/routes')(app, __dirname);
-
-// var eq1 = new Equipment({
-//     name: 'GPS'
-// });
-// var eq2 = new Equipment({
-//     name: 'Szyberdach'
-// });
-// eq1.save(function(err, data) {
-//     if(err) console.log(err);
-// });
-// eq2.save(function(err, data) {
-//     if(err) console.log(err);
-// });
-// var s1 = new Car({
-//     equipment: [eq1._id]
-// });
-// var s2 = new Car({
-//     equipment: [eq2._id]
-// });
-// s1.save(function(err, data) {
-//     if(err) console.log(err);
-// });
-// s2.save(function(err, data) {
-//     if(err) console.log(err);
-// });
 
 app.listen(port);
 console.log("App listening on port " + port);
