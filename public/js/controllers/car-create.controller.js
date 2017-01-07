5c6dd1ebe70f626ae9a05b462dfb15b14c89f6ff
Radosław Chełstowski <chelstowski.radoslaw@gmail.com>
2017-01-07 13:59:17 +0100

    equipment tag

--- public/js/controllers/car-create.controller.js
+++ public/js/controllers/car-create.controller.js
@@ -1,6 +1,6 @@
 angular.module('CarCreateCtrl', [])
 .controller('CarCreateController', ['$scope', '$location', 'CarFactory', 'EquipmentFactory', function($scope, $location, CarFactory, EquipmentFactory) {
-
+    $scope.car = { equipment: [] };
     showEquipments();
 
     $scope.create = function create() {
@@ -14,9 +14,19 @@ angular.module('CarCreateCtrl', [])
     function showEquipments() {
         EquipmentFactory.getEquipments().then(function(response) {
             $scope.equipments = response.data;
+            $scope.eq = $scope.equipments[0];
         }).catch(function(response) {
             console.log(response);
         });
     }
+
+    $scope.removeTheEquipmentFromTheCar = function removeTheEquipmentFromTheCar(equipment) {
+        var index = $scope.car.equipment.indexOf(equipment);
+        $scope.car.equipment.splice(index, 1);
+    };
+
+    $scope.addTheEquipmentToTheCar = function addTheEquipmentToTheCar() {
+        $scope.car.equipment.push($scope.eq);
+    };
 
 }]);
--- public/js/controllers/car-detail.controller.js
+++ public/js/controllers/car-detail.controller.js
@@ -15,9 +15,19 @@ angular.module('CarDetailCtrl', [])
     function showEquipments() {
         EquipmentFactory.getEquipments().then(function(response) {
             $scope.equipments = response.data;
+            $scope.eq = $scope.equipments[0];
         }).catch(function(response) {
             console.log(response);
         });
     }
+
+    $scope.removeTheEquipmentFromTheCar = function removeTheEquipmentFromTheCar(equipment) {
+        var index = $scope.car.equipment.indexOf(equipment);
+        $scope.car.equipment.splice(index, 1);
+    };
+
+    $scope.addTheEquipmentToTheCar = function addTheEquipmentToTheCar() {
+        $scope.car.equipment.push($scope.eq);
+    };
 
 }]);
